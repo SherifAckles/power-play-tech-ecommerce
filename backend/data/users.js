b4fb1e93f8741aa26041d60b1888196d7a4c89fc
nabesteron <49988463+nabesteron@users.noreply.github.com>
2023-06-29 17:25:06 -0500

    created the dummy users data, admin user and 2 other users

--- backend/data/users.js
+++ backend/data/users.js
@@ -1,4 +1,30 @@
 //1-hash password with bcryptjs for Security,Protection against rainbow table attacks,
 //and Adaptability to increasing computational power
+import bcrypt from "bcryptjs";
 
 //2-creating dummy users connected to products
+const users = [
+  {
+    name: "Admin User",
+    email: "admin@email.com",
+    //num is higher=more security=longer period the hash will take
+    password: bcrypt.hashSync("123456", 10),
+    isAdmin: true,
+  },
+  {
+    name: "John Doe",
+    email: "john@email.com",
+    //num is higher=more security=longer period the hash will take
+    password: bcrypt.hashSync("123456", 10),
+    isAdmin: false,
+  },
+  {
+    name: "Jane Doe",
+    email: "jane@email.com",
+    //num is higher=more security=longer period the hash will take
+    password: bcrypt.hashSync("123456", 10),
+    isAdmin: false,
+  },
+];
+
+export default users;
