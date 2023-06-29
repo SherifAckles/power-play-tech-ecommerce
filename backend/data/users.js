601f452233206df4b87b8979e156898264dfef00
nabesteron <49988463+nabesteron@users.noreply.github.com>
2023-06-29 16:34:03 -0500

    preparing dummy users data, and hashing password using bcryptjs

---
+++ backend/data/users.js
@@ -0,0 +1,4 @@
+//1-hash password with bcryptjs for Security,Protection against rainbow table attacks,
+//and Adaptability to increasing computational power
+
+//2-creating dummy users connected to products
