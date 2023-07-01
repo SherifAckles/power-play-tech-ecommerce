2887d28b13d414facc492414565ee423bd450791
nabesteron <49988463+nabesteron@users.noreply.github.com>
2023-07-01 16:49:00 -0500

    commented the asynchandler file

--- backend/middleware/asyncHandler.js
+++ backend/middleware/asyncHandler.js
@@ -1,4 +1,6 @@
-const asyncHandler = fn => (req,res,next)=>{
+//by this we handle errors throw express without creating many trys and catchs
+
+const asyncHandler = fn => (req, res, next) => {
     Promise.resolve(fn(req, res, next)).catch(next);
 }
 
