19bba64f321cc4b367eb4f5296bbbdc2cd218e89
nabesteron <49988463+nabesteron@users.noreply.github.com>
2023-07-02 08:51:20 -0500

    defined the whole constants I will use in redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,21 +6,23 @@ import "./assets/styles/index.css";
 // CUSTOM BOOTSTRAP CSS FILE
 import "./assets/styles/bootstrap.custom.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
   RouterProvider,
 } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<App />}>
-      <Route index={true} path="/" element={<HomeScreen />} />
-      <Route path="/product/:id" element={<ProductScreen />} />
+    <Route path='/' element={<App />}>
+      <Route index={true} path='/' element={<HomeScreen />} />
+      <Route path='/product/:id' element={<ProductScreen />} />
     </Route>
   )
 );
@@ -28,8 +30,8 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
-
-reportWebVitals();
