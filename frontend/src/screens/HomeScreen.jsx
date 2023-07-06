aae282a581e21d948658fca48f2830c7607a7865
nabesteron <49988463+nabesteron@users.noreply.github.com>
2023-07-06 17:56:14 -0500

    created the loader and the error message components

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -2,7 +2,8 @@
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
 import { useGetProductsQuery } from '../slices/productsApiSlice'
-
+import Loader from '../components/Loader';
+import Message from '../components/Message'
 const HomeScreen = () => {
   
 const { data: products, isLoading, error } = useGetProductsQuery()
@@ -12,9 +13,9 @@ const { data: products, isLoading, error } = useGetProductsQuery()
     <>
       
       {isLoading ? (
-      <h2>Loading...</h2>
+      <Loader/>
       ) : error ? (
-          <div>{error?.data.message || error.error }</div>
+          <Message variant='danger'>{error?.data.message || error.error }</Message>
       ):(<>
       <h1>Latest Products</h1>
       <Row>
