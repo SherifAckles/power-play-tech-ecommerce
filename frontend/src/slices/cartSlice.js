aab8ba4110ec187915447ed91f4d4539aba3fd39
nabesteron <49988463+nabesteron@users.noreply.github.com>
2023-07-06 19:05:14 -0500

    calculated the prices

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -4,10 +4,51 @@ const initialState = localStorage.getItem("cart")
   ? JSON.parse(localStorage.getItem("cart"))
   : { cartItems: [] };
 
+const addDecimals = (num) => {
+  return (Math.floor(num * 100) / 100).toFixed(2);
+};
+
 const cartSlice = createSlice({
   name: "cart",
   initialState,
-  reducers: {},
+  reducers: {
+    addToCart: (state, action) => {
+      const item = action.payload;
+      const existItem = state.cartItems.find(
+        (currentItem) => currentItem._id === item._id
+      );
+      if (existItem) {
+        state.cartItems = state.cartItems.map((currentItem) =>
+          currentItem._id === existItem._id ? item : currentItem
+        );
+      } else {
+        state.cartItems = [...state.cartItems, item];
+      }
+
+      //calculate item price
+      state.itemPrice = addDecimals(
+        state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+      );
+      //calculate the shipping price(order over $100 then free,else $10)
+      state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
+
+      //calculate the tax price(15% tax)
+      state.taxPrice = addDecimals(
+        Number((0.15 * state.itemsPrice).toFixed(2))
+      );
+
+      //calculate the total price
+      state.totalPrice = (
+        Number(state.itemsPrice) +
+        Number(state.shippingPrice) +
+        Number(state.taxPrice)
+      ).toFixed(2);
+      //save all prices into the local storage
+      localStorage.setItem("cart", JSON.stringify(state));
+    },
+  },
 });
+
+export const { addToCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
